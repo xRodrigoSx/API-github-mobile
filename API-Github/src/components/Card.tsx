ec7527212b7d38cedc0a1c842cbf4abf617fef72
Rodrigo Santos <rodrgosantos123@hotmail.com>
2024-04-26 21:42:09 -0300

    Senac Update 26.04

--- API-Github/src/components/Card.tsx
+++ API-Github/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, Image, Button } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -12,26 +12,35 @@ interface Props {
 const Card = ({ repository }: Props) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.fullname}>{repository.full_name.split(repository.owner.login+'/')}</Text>
+      <Text style={styles.fullname}>{repository.full_name.split(repository.owner.login + '/')}</Text>
       <Text style={styles.description}>{repository.description}</Text>
-      <View>
-      <Image source={{ uri: repository.owner.avatar_url }} style={styles.avatar} />
-      <Text style={styles.login}>{repository.owner.login}</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+        <Image source={{ uri: repository.owner.avatar_url }} style={styles.avatar} />
+      <View style={{ flexDirection: 'column', marginVertical: 0 }}>
+        <Text style={styles.login}>{repository.owner.login}</Text>
+      <View style={{ flexDirection: 'row', gap: 15, justifyContent: 'center'}}>
+        <Feather name="star" size={24} color="black" />
+        <Text style={styles.stargazers}>{repository.stargazers_count}</Text>
       </View>
-      <Feather name="star" size={24} color="black" />
-      <Text style={styles.stargazers}>{repository.stargazers_count}</Text>
-      <Feather name="eye" size={24} color="black" />
-      <Text style={styles.watchers}>{repository.watchers_count}</Text>
+      <View style={{ flexDirection: 'row', gap: 15, justifyContent: 'center'}}>
+        <Feather name="eye" size={24} color="black" />
+        <Text style={styles.watchers}>{repository.watchers_count}</Text>
+      </View>
+      <Text style={{ color: '#000', fontSize: 16, fontWeight: 'bold', marginTop: 5 }}>Ver mais</Text>
+      </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 10,
     margin: 10,
     borderRadius: 10,
     backgroundColor: "#fff",
+    elevation: 5
   },
   fullname: {
     fontSize: 20,
@@ -39,22 +48,23 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 16,
+    fontStyle: "italic",
+    marginBottom: 10,
   },
   login: {
-    position: "absolute",
-    bottom: 0,
+    bottom: 5,
     right: 0,
   },
   avatar: {
+    bottom: 8,
+    borderRadius: 15,
     width: 100,
     height: 100,
   },
   stargazers: {
-    position: "absolute",
     right: 0,
   },
   watchers: {
-    position: "absolute",
     right: 0,
   }
 });
--- API-Github/src/screens/User.tsx
+++ API-Github/src/screens/User.tsx
@@ -24,11 +24,8 @@ const User = () => {
 
     return (
         <View style={styles.container}>
-            <Text>Buscador de Github</Text>
-            <TextInput value={userInput} onChangeText={setUserInput} placeholder="Escreva o nome de um usuário do Github" />
-            <TouchableOpacity onPress={handleSearchUser}>
-                <Text>Buscar</Text>
-            </TouchableOpacity>
+            <Text style={styles.title}>Buscador de Github</Text>
+            <TextInput style={styles.input} value={userInput} onChangeText={setUserInput} placeholder="Escreva o nome de um usuário do Github" />
             {repositoryList.length > 0 ? (
                 <FlatList
                     data={repositoryList}
@@ -36,20 +33,48 @@ const User = () => {
                     keyExtractor={(item) => item.id.toString()}
                 />
             ) : (
-                <Text>Sem Informações</Text>
+                <Text style={styles.response}>Sem Informações</Text>
             )}
+            <TouchableOpacity style={styles.botao} onPress={handleSearchUser}>
+                <Text style={styles.botao}>Buscar</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    padding: 40,
-},
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        padding: 40,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    botao: {
+        backgroundColor: 'gray',
+        color: 'white',
+        fontSize: 20,
+        padding: 5,
+        borderRadius: 5,
+    },
+    response: {
+        fontSize: 16,
+        marginBottom: 10
+        
+    },
+    input: {
+        fontSize: 14,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+    }
 
 });
 
